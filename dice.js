3a8650e2df72047999fe39a0dd6d83a62f358fe4
Stephen-254 <stephenmutheu@gmail.com>
2024-01-31 17:38:33 +0300

    m

---
+++ dice.js
@@ -0,0 +1,9 @@
+let player1 = prompt("Enter your name");
+let player2 = prompt("Enter your name");
+
+document.querySelectorAll('.players h1')[0].textContent = player1;
+document.querySelectorAll('.players h1')[1].textContent = player2;
+
+
+
+
