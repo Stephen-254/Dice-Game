b77e939b9a97a9ad2e62fb5c4940ab9dcabf6fe9
Stephen-254 <stephenmutheu@gmail.com>
2024-02-17 17:20:57 +0300

    m

--- dice.js
+++ dice.js
@@ -1,9 +1,8 @@
-let player1 = prompt("Enter your name");
-let player2 = prompt("Enter your name");
+let player1 = prompt("Enter player 1's name");
+let player2 = prompt("Enter player 2's name");
 
 document.querySelectorAll('.players h1')[0].textContent = player1;
 document.querySelectorAll('.players h1')[1].textContent = player2;
 
-
-
+document.getElementById('btn').addEventListener('click', rollDice);
 
